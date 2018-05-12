25aa7ee031cf943d7a9f9b21b3a7020e1a40b3c6
Faiz Febriansyah <39045632+faizfeb1998@users.noreply.github.com>
2018-05-12 14:17:01 +0700

    Add files via upload

--- php/login.php
+++ php/login.php
@@ -1,10 +1,15 @@
 <!DOCTYPE HTML>
 <html>
+	<head>
+		<link rel="stylesheet" href="style.css">
+	</head>
 	<body>
+		<h1>Sistem Kepegawaian</h1>
 		<form action="proses_login.php" method="post">
+			<h2>LOGIN</h2>
 			<input type="text" name="username" placeholder="username"/><br>
 			<input type="password" name="password" placeholder="password"/><br>
-			<input type="submit" name="login">	
+			<input class="submit"type="submit" name="login">
 		</form>
 	</body>
 </html>
