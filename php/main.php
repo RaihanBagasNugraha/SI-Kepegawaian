ea4ddd7e695a747de92eb510eb0f0040f6ddb6ef
RaihanBagasNugraha <34880815+RaihanBagasNugraha@users.noreply.github.com>
2018-05-12 20:50:43 +0700

    Update main.php

--- php/main.php
+++ php/main.php
@@ -2,14 +2,19 @@
 include "koneksi.php";
 ?>
 <html>
+<title>Data Pegawai</title>
 <body>
+<link rel="stylesheet" href="style2.css">
 <!--<p>Form Search</p> -->
-<form action="main.php" method="get">
+<form action="pegawai.php" method="get" class="cari">
 	<label>Cari :</label>
 	<input type="text" name="cari">
 	<input type="submit" value="Cari">
+	
+	
 </form>
-<form action="pegawai.php" method="post">   
+
+<form action="pegawai.php" method="post" class="sort2">   
  <select name="sort">  
  <option value="nomor">Sort By</option>  
  <option value="nama">Nama</option>  
@@ -18,8 +23,8 @@ include "koneksi.php";
  </select>   
  <input type="submit" name="enter" value="Enter">   
 </form> 
+<a href="login.php"><button class="btn2">Login</button></a></br>
 <!--<p>Table Pegawai</p> -->
-<a href = login.php>Login</a>
 <table border = "1">
 	<tr>
 	 <th>Nama</th>
@@ -31,17 +36,17 @@ include "koneksi.php";
 	if(isset($_GET['cari'])){
 		$cari = $_GET['cari'];
 		$query = mysqli_query($koneksi,"select * from pegawai where nama like '%".$cari."%' OR nip like '%".$cari."%'
-		OR tgl_lahir like '%".$cari."%' OR divisi like '%".$cari."%'");				
+		OR tgl_lahir like '%".$cari."%' OR divisi like '%".$cari."%'");		
 	}
 	else if (isset($_POST['sort']))
 	{
     $sort = $_POST['sort'];
-	echo "Ordered By ".$sort."";
 		$query = mysqli_query($koneksi,"select * from pegawai order by ".$sort."");
 	}
 	else{
 		$query = mysqli_query($koneksi,"select * from pegawai order by nomor");		
 	}
+	
 	$no = 1;
 	while($data = mysqli_fetch_array($query)){
 	?>
