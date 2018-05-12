e43f24590c1a714d0f44fb8c91823ed0f080f0ed
RaihanBagasNugraha <34880815+RaihanBagasNugraha@users.noreply.github.com>
2018-05-12 13:13:03 +0700

    Update pegawai.php
    added sort function

--- php/pegawai.php
+++ php/pegawai.php
@@ -8,7 +8,19 @@ include "koneksi.php";
 	<label>Cari :</label>
 	<input type="text" name="cari">
 	<input type="submit" value="Cari">
+	
 </form>
+
+<form action="pegawai.php" method="post">   
+ <select name="sort">  
+ <option value="nomor">Sort By</option>  
+ <option value="nama">Nama</option>  
+ <option value="nip">NIP</option>    
+ <option value="divisi">Divisi</option>  
+ </select>   
+ <input type="submit" name="enter" value="Enter">   
+</form> 
+
 <!--<p>Table Pegawai</p> -->
 <table border = "1">
 	<tr>
@@ -23,10 +35,18 @@ include "koneksi.php";
 	if(isset($_GET['cari'])){
 		$cari = $_GET['cari'];
 		$query = mysqli_query($koneksi,"select * from pegawai where nama like '%".$cari."%' OR nip like '%".$cari."%'
-		OR tgl_lahir like '%".$cari."%' OR divisi like '%".$cari."%'");				
-	}else{
-		$query = mysqli_query($koneksi,"select * from pegawai");		
+		OR tgl_lahir like '%".$cari."%' OR divisi like '%".$cari."%'");		
 	}
+	else if (isset($_POST['sort']))
+	{
+    $sort = $_POST['sort'];
+	echo "Ordered By ".$sort."";
+		$query = mysqli_query($koneksi,"select * from pegawai order by ".$sort."");
+	}
+	else{
+		$query = mysqli_query($koneksi,"select * from pegawai order by nomor");		
+	}
+	
 	$no = 1;
 	while($data = mysqli_fetch_array($query)){
 	?>
@@ -42,5 +62,7 @@ include "koneksi.php";
 	}
 	?>
 </table>
+	<a href=tambah_data.php>Tambah Data</a>
+	<a href=index.php>Menu</a>
 </body>
 </html>
