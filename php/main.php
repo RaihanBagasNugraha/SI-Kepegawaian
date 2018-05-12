51765e66c511a7d903f7a419022b8e4df5aabdfa
RaihanBagasNugraha <34880815+RaihanBagasNugraha@users.noreply.github.com>
2018-05-12 13:13:44 +0700

    Update main.php
    edited

--- php/main.php
+++ php/main.php
@@ -9,6 +9,15 @@ include "koneksi.php";
 	<input type="text" name="cari">
 	<input type="submit" value="Cari">
 </form>
+<form action="pegawai.php" method="post">   
+ <select name="sort">  
+ <option value="nomor">Sort By</option>  
+ <option value="nama">Nama</option>  
+ <option value="nip">NIP</option>    
+ <option value="divisi">Divisi</option>  
+ </select>   
+ <input type="submit" name="enter" value="Enter">   
+</form> 
 <!--<p>Table Pegawai</p> -->
 <a href = login.php>Login</a>
 <table border = "1">
@@ -23,8 +32,15 @@ include "koneksi.php";
 		$cari = $_GET['cari'];
 		$query = mysqli_query($koneksi,"select * from pegawai where nama like '%".$cari."%' OR nip like '%".$cari."%'
 		OR tgl_lahir like '%".$cari."%' OR divisi like '%".$cari."%'");				
-	}else{
-		$query = mysqli_query($koneksi,"select * from pegawai");		
+	}
+	else if (isset($_POST['sort']))
+	{
+    $sort = $_POST['sort'];
+	echo "Ordered By ".$sort."";
+		$query = mysqli_query($koneksi,"select * from pegawai order by ".$sort."");
+	}
+	else{
+		$query = mysqli_query($koneksi,"select * from pegawai order by nomor");		
 	}
 	$no = 1;
 	while($data = mysqli_fetch_array($query)){
