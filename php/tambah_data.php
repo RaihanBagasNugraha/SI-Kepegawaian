d50515ae1193661c9f3efc2a5512848383e103ff
RaihanBagasNugraha <34880815+RaihanBagasNugraha@users.noreply.github.com>
2018-05-12 20:52:08 +0700

    Update tambah_data.php

--- php/tambah_data.php
+++ php/tambah_data.php
@@ -1,12 +1,15 @@
 <html>
 <body>
-	<h2>Tambah Data Pegawai<h2>
+	<title>Tambah Data</title>
+	<h1>Tambah Data Pegawai<h1>
+	<link rel="stylesheet" href="style.css">
 	<form action="proses_input.php" method="post">
 		<input type="text" name="nama" placeholder="NAMA"><br>
 		<input type="text" name="nip" placeholder="NIP"><br>
 		<input type="text" name="tgl_lahir" placeholder="Tanggal Lahir"><br>
 		<input type="text" name="divisi" placeholder="DIVISI"><br>
-		<input type="submit"value="kirim">
+		<input type="submit" value="Submit">
 	</form>
+	<a href="index.php"><button class="menu">Menu</button></a></br>
 </body>
 </html>
