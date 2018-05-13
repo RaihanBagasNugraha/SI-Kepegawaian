6f4aa34d0db25fffea9bc29ea6755ccdc2279db5
RaihanBagasNugraha <34880815+RaihanBagasNugraha@users.noreply.github.com>
2018-05-13 21:37:23 +0700

    Update index.php

--- php/index.php
+++ php/index.php
@@ -7,12 +7,30 @@ if(!isset($_SESSION['username'])){
 ?>
 <!DOCTYPE HTML>
 <html>
+<head>
+<style type="text/css">
+		body {
+			background: url('office.jpg');
+			color: #fff;
+			background-size: 1200px 720px;
+		}
+		h1{
+			color:black;
+		}
+		h2{
+			color:white;
+		}
+</style>
 <title>Index</title>
-		<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="style.css">
+</head>
+	<body>
 		<h1>Welcome Admin</h1>
 		<br>
 		<a href="tambah_data.php"><button class="tambah">Tambah Data Pegawai</button></a></br>
 		<a href="pegawai.php"><button type="button" class="edit">Edit Data Pegawai</button></a></br>
-		<a href="logout.php"><button type="button" class="out">Logout</button></a>
+		<a href="logout.php"><button type="button" class="out">Logout</button></a><br>
+		<br>
+		<h2><marquee scrollamount="10">Selamat Datang di Sistem Kepegawaian Ars Corporation.Co.Ltd</marquee></h2>
 	</body>
 </html>
