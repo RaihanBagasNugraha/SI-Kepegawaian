243d970f193089836c5a1fe48b464d4e0bcd1531
RaihanBagasNugraha <34880815+RaihanBagasNugraha@users.noreply.github.com>
2018-05-13 21:35:28 +0700

    Update login.php

--- php/login.php
+++ php/login.php
@@ -3,9 +3,20 @@
 	<head>
 		<title>LOGIN sistem Kepegawaian</title>
 		<link rel="stylesheet" href="style.css">
+		<style type="text/css">
+		body {
+			background: url('office.jpg');
+			color: #fff;
+			background-size: 1200px 720px;
+		}
+		h1{
+			color:black;
+		}
+		</style>
 	</head>
 	<body>
-		<h1>Sistem Kepegawaian</h1>
+		<h1>Sistem Kepegawaian Ars Corp</h1>
+		<br>
 		<form action="proses_login.php" method="post">
 			<h2>LOGIN</h2>
 			<input type="text" name="username" placeholder="username"/><br>
