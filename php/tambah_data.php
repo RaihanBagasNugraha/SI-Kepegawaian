d082f5c1f16c7a941eac8be8cd7d4144eafffe46
RaihanBagasNugraha <34880815+RaihanBagasNugraha@users.noreply.github.com>
2018-05-13 21:37:55 +0700

    Update tambah_data.php

--- php/tambah_data.php
+++ php/tambah_data.php
@@ -1,8 +1,23 @@
 <html>
 <body>
+<head>
+<style type="text/css">
+		body {
+			background: url('office.jpg');
+			color: #fff;
+			background-size: 1200px 720px;
+		}
+		h1{
+			color:black;
+		}
+		h2{
+			color:white;
+		}
+</style>
 	<title>Tambah Data</title>
-	<h1>Tambah Data Pegawai<h1>
 	<link rel="stylesheet" href="style.css">
+</head>
+	<h1>Tambah Data Pegawai<h1>
 	<form action="proses_input.php" method="post">
 		<input type="text" name="nama" placeholder="NAMA"><br>
 		<input type="text" name="nip" placeholder="NIP"><br>
